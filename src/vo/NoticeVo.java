0928fb914a276d2995c8d7b69afd3b6dbddb01f7
han yo han <fmfmfmgk@hanmail.net>
2023-12-11 13:32:09 +0900

    2023-12-11 1:31 수정

---
+++ src/vo/NoticeVo.java
@@ -0,0 +1,56 @@
+package vo;
+
+public class NoticeVo {
+	 private String notice_no;
+	 private String notice_title;
+	 private String notice_content;
+	 private int notice_view;
+	 private String notice_del;
+	 private String notice_date;
+	 private String admin_no;
+	 
+	public String getNotice_no() {
+		return notice_no;
+	}
+	public void setNotice_no(String notice_no) {
+		this.notice_no = notice_no;
+	}
+	public String getNotice_title() {
+		return notice_title;
+	}
+	public void setNotice_title(String notice_title) {
+		this.notice_title = notice_title;
+	}
+	public String getNotice_content() {
+		return notice_content;
+	}
+	public void setNotice_content(String notice_content) {
+		this.notice_content = notice_content;
+	}
+	public int getNotice_view() {
+		return notice_view;
+	}
+	public void setNotice_view(int notice_view) {
+		this.notice_view = notice_view;
+	}
+	public String getNotice_del() {
+		return notice_del;
+	}
+	public void setNotice_del(String notice_del) {
+		this.notice_del = notice_del;
+	}
+	public String getNotice_date() {
+		return notice_date;
+	}
+	public void setNotice_date(String notice_date) {
+		this.notice_date = notice_date;
+	}
+	public String getAdmin_no() {
+		return admin_no;
+	}
+	public void setAdmin_no(String admin_no) {
+		this.admin_no = admin_no;
+	}
+	 
+	 
+}
